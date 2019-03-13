d07fe1ab6688519e581679b7ba5c4c264af9156d
Dean Landis <dean.landis.02@gmail.com>
2019-03-12 21:09:24 -0500

    to get word prompts to do my wrting

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+    }
+
+    onSave() {
+        console.log('asdf');
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    My name is Dean and this is a witing prompts website. You might get some crazy and some lame or some
+                    mild.
+                </header>
+
+                <section>
+                    <section>
+                        prompt nmhvnhg adsf adsf dffdsmfsdamfsdmsdfamsdfamsdfmsadfmsdfamsdfamsadfmmnfdsa sdfa as fd sadf
+                    </section>
+                    <section>
+                        <MyEditor/>
+                    </section>
+                    <section>
+                        <button onClick={() => this.onSave()}>Save</button>
+                    </section>
+                </section>
+            </div>
+        );
+    }
 }
+
+class MyEditor extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {text: ""};
+    }
+
+    render() {
+        return (
+            <ReactQuill
+                value={this.state.text}
+                onChange={this.handleChange}/>
+        );
+    }
+}
+
+const styles = {
+    editor: {
+        border: '1px solid gray',
+        minHeight: '6em'
+    }
+};
+
 
 export default App;

6656e716bf3b7331f9ee9e2dc8098878e9590336
Nathan Landis <my8bird@gmail.com>
2019-03-25 20:28:39 -0500

    New Text button

--- src/App.js
+++ src/App.js
@@ -8,26 +8,42 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Button from 'react-bootstrap/Button';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      text: localStorage.getItem('text'),
-      prompt: ""
+      text: localStorage.getItem('text') || '',
+      prompt: localStorage.getItem('prompt') || ''
     };
   }
 
   componentDidMount() {
-    fetch('https://www.ineedaprompt.com/dictionary/default/prompt?q=adj+noun+adv+verb+noun+location')
-    .then((response) => response.json())
-    .then((prompt) => this.setState({text: this.state.text, prompt: prompt.english}));
-
+      if(this.state.prompt.length===0) {
+        this.getNewPrompt();
+      }
   }
+
+  getNewPrompt() {
+      fetch('https://www.ineedaprompt.com/dictionary/default/prompt?q=adj+noun+adv+verb+noun+location')
+      .then((response) => response.json())
+      .then((prompt) => {
+        this.setState({text: this.state.text, prompt: prompt.english});
+        localStorage.setItem('prompt', prompt.english);
+      });
+    }
 
   onSave() {
     localStorage.setItem('text', this.state.text);
+  }
+
+  onNewText() {
+    localStorage.clear();
+    this.setState({text: '', prompt: ''});
+    this.getNewPrompt();
   }
 
   handleChange(newText) {
@@ -35,17 +51,21 @@ class App extends Component {
   }
 
   render() {
+    let now = ((this.state.text.length)/500) * 100;
     return (
       <>
   <Navbar bg="dark" variant="dark">
     <Navbar.Brand href="#home">
-      {'Deans writing prompts'}
+      Deans writing prompts
     </Navbar.Brand>
-
+    <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+      <Button onClick={() => this.onNewText()}>New Text</Button>
+      </Navbar.Collapse>
   </Navbar>
       <Container>
 
-        <Row className="b">
+        <Row>
           <Col>
             {this.state.prompt}
           </Col>
@@ -62,7 +82,13 @@ class App extends Component {
 
         <Row>
           <Col >
-            <button onClick={() => this.onSave()}>Save</button>
+            <ProgressBar animated now={now} label={`${now}%`} />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col >
+            <Button onClick={() => this.onSave()}>Save</Button>
           </Col>
         </Row>
       </Container>
